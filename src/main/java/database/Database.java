28e8eb4e6116bbde164bb8dcd2ec98a44a480856
makchich <m.akchich@student.tudelft.nl>
2016-12-20 11:03:30 +0100

    Added insertAuthorizationToken to Database.java.
    This method inserts an authorization token for a device in the database.

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -121,4 +121,16 @@ public final class Database {
         }
         return result;
     }
+
+    /**
+     * Inserts the Authorization token of a device requesting a token in the database.
+     *
+     * @param id    the UUID of the device
+     * @param token the generated authorization token for the device
+     * @return <code>true</code> if successfully added, otherwise <code>false</code>
+     */
+    public static boolean insertAuthorizationToken(String id, String token) {
+        String sql = "INSERT INTO `device` VALUES(?, ?)";
+        return executeManipulationQuery(sql, id, token);
+    }
 }
