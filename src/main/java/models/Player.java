cdf6c43232ddc88b1e81adbb646902a1bbc13c05
Thomas Overklift <t.a.r.overkliftvaupelklein@student.tudelft.nl>
2016-12-20 10:28:07 +0100

    Fixed checkstyle warnings

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -5,13 +5,19 @@ import database.SessionQueries;
 import static database.SessionQueries.generateUniqueID;
 
 /**
- * Model of the player
+ * Model of the player.
  */
 public class Player {
 
     private String playerID, sessionID, deviceID, roleName;
     private Integer roleID, score;
 
+    /**
+     * Constructor for the player class.
+     * @param playerID A player ID.
+     * @param sessionID A session ID.
+     * @param deviceID A device ID.
+     */
     public Player(String playerID, String sessionID, String deviceID) {
         this.playerID = playerID;
         this.sessionID = sessionID;
@@ -33,22 +39,43 @@ public class Player {
         return player;
     }
 
+    /**
+     * Gets the player ID.
+     * @return The player ID.
+     */
     public String getPlayerID() {
         return playerID;
     }
 
+    /**
+     * Gets the session ID.
+     * @return The session ID.
+     */
     public String getSessionID() {
         return sessionID;
     }
 
+    /**
+     * Gets the device ID.
+     * @return The device ID.
+     */
     public String getDeviceID() {
         return deviceID;
     }
 
+    /**
+     * Allows the role of the player to be set.
+     * @param roleID The role ID.
+     */
     protected void setRoleID(Integer roleID) {
         this.roleID = roleID;
     }
 
+
+    /**
+     * Gets the role ID.
+     * @return The role ID.
+     */
     public int getRoleID() {
         if (roleID == null) {
             roleID = SessionQueries.getRoleId(this);
@@ -56,6 +83,10 @@ public class Player {
         return roleID;
     }
 
+    /**
+     * Gets the role ID by name.
+     * @return The role name.
+     */
     public String getRole() {
         if (roleName == null) {
             roleName = Role.getById(getRoleID()).name();
@@ -63,14 +94,26 @@ public class Player {
         return roleName;
     }
 
+    /**
+     * Returns whether this player is an admin or not.
+     * @return The boolean value.
+     */
     public boolean isAdmin() {
-        return getRoleID()==0;
+        return getRoleID() == 0;
     }
 
+    /**
+     * Allows the score of the player to be set.
+     * @param score The amount of points this player has scored.
+     */
     protected void setScore(Integer score) {
         this.score = score;
     }
 
+    /**
+     * Gets the score.
+     * @return The score.
+     */
     public int getScore() {
         if (score == null) {
             score = SessionQueries.getScore(this);
