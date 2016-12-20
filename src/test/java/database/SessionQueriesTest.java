41ccb4ccc5f1a38e0c6b3262b46ecd752b7f1b98
Thomas Overklift <t.a.r.overkliftvaupelklein@student.tudelft.nl>
2016-12-20 13:52:40 +0100

    Tested Player and Role models

---
+++ src/test/java/database/SessionQueriesTest.java
@@ -0,0 +1,52 @@
+package database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Thomas on 20-12-2016.
+ */
+public class SessionQueriesTest {
+
+    @Test
+    public void createSession() throws Exception {
+
+    }
+
+    @Test
+    public void getNewPlayerOfSession() throws Exception {
+
+    }
+
+    @Test
+    public void getRoleId() throws Exception {
+
+    }
+
+    @Test
+    public void getScore() throws Exception {
+
+    }
+
+    @Test
+    public void getSessionStatus() throws Exception {
+
+    }
+
+    @Test
+    public void updateSessionStatus() throws Exception {
+
+    }
+
+    @Test
+    public void generateUniqueID() throws Exception {
+
+    }
+
+    @Test
+    public void generateUniqueJoinToken() throws Exception {
+
+    }
+
+}
--- src/test/java/validation/ValidationTest.java
+++ src/test/java/validation/ValidationTest.java
@@ -1,7 +1,5 @@
 package validation;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Constructor;
