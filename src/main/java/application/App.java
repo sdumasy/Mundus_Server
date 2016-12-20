291d05ec9905c73af434c6d85a3a1d1f7fd93649
Stephan <stephan_dumasy@icloud.com>
2016-12-20 23:07:11 +0100

    herooku

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import http.Routes;
 
+import static spark.Spark.port;
+
 /**
  * Created by macbookpro on 29/11/2016.
  */
@@ -16,6 +18,15 @@ public final class App {
      * @param args Runtime args.
      */
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         Routes.setupRoutes();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
