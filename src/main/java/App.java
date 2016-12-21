4c24b465007495ff3b8d3825be8686bc0f47a733
makchich <m.akchich@student.tudelft.nl>
2016-12-21 02:01:31 +0100

    Modified pom.xml to support Heroku.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import http.Routes;
 
+import static spark.Spark.port;
+
 /**
  * Created by macbookpro on 29/11/2016.
  */
@@ -7,13 +9,24 @@ public final class App {
     /**
      * Private constructor.
      */
-    private App() { }
+    private App() {
+    }
 
     /**
      * Main method.
+     *
      * @param args Runtime args.
      */
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         Routes.setupRoutes();
+    }
+
+    public static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
