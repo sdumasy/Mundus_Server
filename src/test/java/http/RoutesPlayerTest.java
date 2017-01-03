04e98c851268ce0b10956a879e7d9e9a0f14805b
Thomas Overklift <t.a.r.overkliftvaupelklein@student.tudelft.nl>
2017-01-03 19:20:25 +0100

    Improve get all players test.

--- src/test/java/http/RoutesPlayerTest.java
+++ src/test/java/http/RoutesPlayerTest.java
@@ -20,10 +20,10 @@ import spark.Spark;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import static http.RoutesTest.processAuthorizedGetRoute;
-import static http.RoutesTest.processAuthorizedPostRoute;
-import static http.RoutesTest.processAuthorizedPutRoute;
+import static http.RoutesTest.*;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -60,7 +60,12 @@ public class RoutesPlayerTest {
     @Test
     public void setupGetAllPlayersTest() throws IOException {
         PowerMockito.mockStatic(PlayerQueries.class);
-
+        Device device = new Device("Device_ID", MOCKED_TOKEN);
+        Session session = new Session("", "", 1, LocalDateTime.now());
+        List players = new ArrayList<Player>();
+        players.add(new Player("1", session, device, Role.Admin, 0, ""));
+        players.add(new Player("2", session, device, Role.Admin, 0, ""));
+        when(PlayerQueries.getAllPlayers(any())).thenReturn(players);
         String uri = "/player/all";
         HttpResponse response = processAuthorizedGetRoute(uri, new Device("Device_ID", MOCKED_TOKEN));
         assertEquals("HTTP/1.1 200 OK", response.getStatusLine().toString());
